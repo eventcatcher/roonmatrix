24afaf90d9b1ca0c62235bc5d7cfa84dfd5cba92
Stephan Wilhelm <stephan.wilhelm@posteo.de>
2026-01-25 17:25:26 +0100

    windowResizeToFullWidthAndMinimumHeight : fix width on displays with scaleFactor != 1

--- lib/ui/main/main_bloc.dart
+++ lib/ui/main/main_bloc.dart
@@ -1624,7 +1624,9 @@ class MainBloc extends Bloc<MainEvent, MainState> {
     required Size minDesktopSize,
   }) async {
     Display primaryDisplay = await getPrimaryDisplay();
-    Size newSize = Size(primaryDisplay.size.width, minDesktopSize.height + 10);
+    Size newSize = Size(
+        primaryDisplay.size.width / (primaryDisplay.scaleFactor ?? 1),
+        minDesktopSize.height + 10);
 
     await windowManager.setPosition(Offset.zero);
     windowManager.setSize(newSize, animate: true);
