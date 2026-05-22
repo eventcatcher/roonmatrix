cd0d92f6c8b9b5c2249595ab0907afea7e8a5ce7
Stephan Wilhelm <stephan.wilhelm@posteo.de>
2026-05-22 07:45:04 +0200

    fix windows backend assets path in bat file

--- build_windows_py_packages.bat
+++ build_windows_py_packages.bat
@@ -3,7 +3,7 @@
 set "SERIOUS_PYTHON_SITE_PACKAGES=C:\Users\swilhelm\dev\gits\roonmatrix\app\src\__pypackages__"
 
 cd packages/python_backend
-dart run serious_python:main package ../../app/src -p Windows --asset packages/python_backend/assets/backend/roonmatrix.zip -r -r -r ../../app/src/requirements.txt
+dart run serious_python:main package ../../app/src -p Windows --asset assets/backend/roonmatrix.zip -r -r -r ../../app/src/requirements.txt
 cd ../../
 
 pause
