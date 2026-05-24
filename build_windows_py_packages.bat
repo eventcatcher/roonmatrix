240784a19fafb9e201a2db4fab53f79d5fb7f20d
Stephan Wilhelm <stephan.wilhelm@posteo.de>
2026-05-24 08:11:15 +0200

    Windows bat script: use userprofile var

--- build_windows_py_packages.bat
+++ build_windows_py_packages.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "SERIOUS_PYTHON_SITE_PACKAGES=C:\Users\swilhelm\dev\gits\roonmatrix\app\src\__pypackages__"
+set "SERIOUS_PYTHON_SITE_PACKAGES=%userprofile%\dev\gits\roonmatrix\app\src\__pypackages__"
 
 cd packages/python_backend
 dart run serious_python:main package ../../app/src -p Windows --asset assets/backend/roonmatrix.zip -r -r -r ../../app/src/requirements.txt
