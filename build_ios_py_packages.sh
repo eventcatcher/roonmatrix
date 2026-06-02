d707a5ad1aa884c184099cdab97cf35e866e56f8
Stephan Wilhelm <stephan.wilhelm@posteo.de>
2026-06-02 09:18:32 +0200

    fix ios package build script (remove executables and so files to fulfill app store requirements)

--- build_ios_py_packages.sh
+++ build_ios_py_packages.sh
@@ -3,3 +3,6 @@ rm -rf app/src/__pypackages__/.pod
 cd packages/python_backend
 fvm dart run serious_python:main package ../../app/src -p iOS --asset assets/backend/roonmatrix.zip -r -r -r ../../app/src/requirements.txt
 cd ../../
+
+find app/src/__pypackages__/iphoneos.arm64 -type d -name 'opt' -exec rm -r {} \;
+find app/src/__pypackages__/iphoneos.arm64/PIL -type f -name '*.so' -exec rm {} \;
