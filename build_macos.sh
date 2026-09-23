d58cd7adb03532678372b4294016a971650b1593
Stephan Wilhelm <stephan.wilhelm@posteo.de>
2026-09-23 16:09:12 +0200

    fix build scripts

--- build_macos.sh
+++ build_macos.sh
@@ -13,11 +13,12 @@ export SERIOUS_PYTHON_VERSION=3.14
 export SERIOUS_PYTHON_SITE_PACKAGES=$(pwd)/build/site-packages
 export SERIOUS_PYTHON_APP=$(pwd)/build/app
 export SERIOUS_PYTHON_BUNDLE_ID=de.eventcatcher.roonmatrixmac
+export SERIOUS_PYTHON_VERIFY_PROVIDER_SIGNATURES=require
 cd packages/python_backend
 fvm dart run serious_python:main package ../../app/src -p Darwin -r -r -r ../../app/src/requirements.txt
 cd ../../
 
 #find build/site-packages -type f -name '*.so' -exec rm -f {} +
 
-fvm flutter build macos
+fvm flutter build macos --release
 #fvm flutter run --release
